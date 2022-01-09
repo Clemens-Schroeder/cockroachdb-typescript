d5a50fa0f5ff27fe8d07534e0f753739b346f66b
Clemens Schroeder <clemi.schroeder@gmx.ch>
2022-01-09 08:33:55 +0100

    altered Admin.tsx to allow user input for player field

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -6,22 +6,11 @@ interface Player {
 }
 
 export default function Admin() {
-    const [players, setPlayers] = useState([] as Player[]);
+    const [playor, setPlayer] = useState("");
     const [score, setScore] = useState("");
     const [message, setMessage] = useState("");
     const [selectedPlayer, setSelectedPlayer] = useState<Player | undefined>(undefined);
     
-    useEffect(() => {
-        fetch('/.netlify/functions/getPlayers')
-            .then(response => response.json() as Promise<Player[]>)
-            .then(data => {
-                setPlayers(data);
-                if(data && data.length > 0) {
-                    setSelectedPlayer(data[0]);
-                }
-            });
-    }, []);
-
     const selectPlayer = (players: HTMLSelectElement) => {
         setSelectedPlayer({
             id: players.value,
@@ -31,7 +20,7 @@ export default function Admin() {
 
     const addScore = () => {
         const scoreEntry = {
-            playerId: selectedPlayer?.id,
+            playerId: playor,
             score: score
         }
 
@@ -45,32 +34,25 @@ export default function Admin() {
                 body: JSON.stringify(scoreEntry)
             })
             .then(function(res){
-                setMessage(`Leaderboard score of ${score} added for player ${selectedPlayer?.name}`);
+                setMessage(`Leaderboard score of ${score} added for player ${playor}`);
             })
             .catch(function(res){
-                setMessage(`Unable to add score for player ${selectedPlayer?.name}`);
+                setMessage(`Unable to add score for player ${playor}`);
             })
     }
 
     return <>
         <h2>Admin</h2>
         <h4>Add a Leaderboard Entry</h4>
-        {players.length === 0 ?
-            <div>Loading Players...</div>
-            :
-            <>
+        {
             <div className="mb-3 score-entry">
                 <label>Player</label>
-                <select
-                    className="form-select"
-                    aria-label="player selection"
-                    value={selectedPlayer?.id}
-                    onChange={e => selectPlayer(e.target)}>
-                    {players.map(p => <option key={p.id} value={p.id}>
-                        {p.name}
-                    </option>)}
-                </select>
-            </div>
+                <input type="text"
+                       className="form-control"
+                       aria-label="score entry"
+                       value={playor}
+                       onChange={e => setPlayer(e.target.value)}/>
+            </div>            
             <div className="mb-3 score-entry">
                 <label>Score</label>
                 <input type="text"
@@ -87,4 +69,3 @@ export default function Admin() {
         <div className="admin-message">{message}</div>
     </>
 }
-
